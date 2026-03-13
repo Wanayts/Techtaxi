42a867d93e29d83090030be94074b37f1f0682ad
John Wanayts <johnraymundjuanites@gmail.com>
2026-03-14 06:18:03 +0800

    Update click.js

--- js/click.js
+++ js/click.js
@@ -1,24 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const industriesLink = document.querySelector('.dropdown-hover > a');
-  const navbar = document.querySelector('.navbar');
+const industriesLink = document.querySelector('.dropdown-hover > a');
 
-  industriesLink.addEventListener('click', function(e) {
-    if (window.innerWidth < 992) {
-      e.preventDefault();
-      const menu = this.parentElement.querySelector('.hover-menu');
+industriesLink.addEventListener('click', function(e) {
 
-      // toggle class
-      menu.classList.toggle('show-menu');
-    }
-  });
+  if (window.innerWidth < 992) { // mobile only
+    e.preventDefault();
 
-  // close if clicked outside navbar
-  document.addEventListener('click', function(e) {
-    if (window.innerWidth < 992) {
-      const menu = industriesLink.parentElement.querySelector('.hover-menu');
-      if (!navbar.contains(e.target)) {
-        menu.classList.remove('show-menu');
-      }
-    }
-  });
+    const menu = this.parentElement.querySelector('.hover-menu');
+
+    menu.classList.toggle('show-menu');
+  }
+
 });

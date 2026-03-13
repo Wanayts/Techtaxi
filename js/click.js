b3ae60e463061c9610d35112660f6d619ceecaa0
John Wanayts <johnraymundjuanites@gmail.com>
2026-03-14 05:11:50 +0800

    Update click.js

--- js/click.js
+++ js/click.js
@@ -7,13 +7,3 @@ industriesLink.addEventListener('click', function(e) {
     menu.classList.toggle('show-menu');
   }
 });
-
-// Close when clicking outside
-document.addEventListener('click', function(e) {
-  if (window.innerWidth < 992) {
-    const menu = industriesLink.nextElementSibling;
-    if (!industriesLink.parentElement.contains(e.target)) {
-      menu.classList.remove('show-menu');
-    }
-  }
-});
